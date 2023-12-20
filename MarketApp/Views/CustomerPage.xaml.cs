8fcc4b478c6cb5fb9f9deb5b97982119da20afd9
dasda98 <wojniakjakub@gmail.com>
2023-12-20 20:43:59 +0100

    New View structure. Show customer list

---
+++ MarketApp/Views/CustomerPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using MarketApp.Context;
+using MarketApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace MarketApp.Views
+{
+    /// <summary>
+    /// Interaction logic for CustomerPage.xaml
+    /// </summary>
+    public partial class CustomerPage : Page
+    {
+        private ObservableCollection<Customer> _customers;
+
+        public ObservableCollection<Customer> Customers { 
+            get => _customers; 
+            set
+            {
+                _customers = value;
+            } 
+        }
+
+        public CustomerPage()
+        {
+            DataContext = this;
+            var _context = new DatabaseContext();
+
+            _customers = new ObservableCollection<Customer>(_context.Customers.ToList());
+
+
+            InitializeComponent();
+        }
+    }
+}
