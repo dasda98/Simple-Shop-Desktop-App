2f6691a118740918afccecee99703c06fb5b84be
dasda98 <wojniakjakub@gmail.com>
2023-12-23 13:19:54 +0100

    Add ViewModels

---
+++ MarketApp/ViewModel/CustomerViewModel.cs
@@ -0,0 +1,34 @@
+﻿using MarketApp.Helpers;
+using MarketApp.Interfaces;
+using MarketApp.Models;
+using System.Collections.ObjectModel;
+
+namespace MarketApp.ViewModel
+{
+    public class CustomerViewModel : ViewModelBase
+    {
+        private readonly ICustomerService _customerService;
+
+        private ObservableCollection<Customer> _customers;
+        public ObservableCollection<Customer> Customers { 
+            get {  return _customers; }
+            set {
+                if (value != _customers)
+                {
+                    _customers = value;
+                    OnPropertyChanged(nameof(Customers));
+                }
+            }
+        }
+
+        public CustomerViewModel(ICustomerService customerService) {
+            _customerService = customerService;
+            LoadCustomers();
+        }
+
+        private void LoadCustomers()
+        {
+            _customers = new ObservableCollection<Customer>(_customerService.GetAllCustomers());
+        }
+    }
+}
