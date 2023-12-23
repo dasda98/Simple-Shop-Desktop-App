dfe1a1856ab03ef093164edc6b49562a17a2670a
dasda98 <wojniakjakub@gmail.com>
2023-12-23 13:19:41 +0100

    Register services

--- MarketApp/App.xaml.cs
+++ MarketApp/App.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using MarketApp.Context;
+using MarketApp.Interfaces;
+using MarketApp.Repositories;
+using MarketApp.Services;
+using MarketApp.ViewModel;
+using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
 
 namespace MarketApp
@@ -9,6 +13,34 @@ namespace MarketApp
     /// </summary>
     public partial class App : Application
     {
+
+        public App()
+        {
+            Services = ConfigureServices();
+
+            this.InitializeComponent();
+        }
+        public new static App Current => (App)Application.Current;
+        public IServiceProvider Services { get; }
+
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            services.AddDbContext<DatabaseContext>();
+
+            services.AddTransient<ICustomerService, CustomerService>();
+            services.AddTransient<ICustomerRepository, CustomerRepository>();
+
+            services.AddTransient<CustomerViewModel>();
+
+            return services.BuildServiceProvider();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+        }
     }
 
 }
